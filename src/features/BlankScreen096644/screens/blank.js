ac0a893a4ba9e16a4ed02acbb5ee377471c56617
Crowdbotics <team@crowdbotics.com>
2020-09-19 15:44:19 +0000

    Update components

--- src/features/BlankScreen096644/screens/blank.js
+++ src/features/BlankScreen096644/screens/blank.js
@@ -25,112 +25,145 @@ export class _Blank extends React.Component {
   state = {}
 
   render = () => (
-    <View
-      style={{
-        width: "100%",
-        height: "100%",
-        marginLeft: 0,
-        marginRight: 0,
-        marginTop: 0,
-        marginBottom: 0,
-        paddingLeft: 2,
-        paddingRight: 2,
-        paddingTop: 0,
-        paddingBottom: 0,
-        overflow: "visible",
-        textAlign: "left",
-        verticalAlign: "baseline",
-        fontSize: 12,
-        color: "#000000",
-        backgroundColor: "#ffffff",
-        fontFamily: "Roboto-Regular",
-        flexDirection: "column",
-        flexWrap: "wrap",
-        justifyContent: "flex-start",
-        alignItems: "stretch",
-        alignContent: "stretch",
-        borderColor: "#000000",
-        borderStyle: "solid",
-        borderWidth: 0,
-        borderLeftWidth: 0,
-        borderRightWidth: 0,
-        borderTopWidth: 0,
-        borderBottomWidth: 0,
-        borderRadius: 0,
-        backgroundSize: "auto"
-      }}
-    >
-      <View
-        style={{
-          width: "100%",
-          height: "100%",
-          marginLeft: 0,
-          marginRight: 0,
-          marginTop: 0,
-          marginBottom: 0,
-          paddingLeft: 0,
-          paddingRight: 0,
-          paddingTop: 0,
-          paddingBottom: 0,
-          overflow: "visible",
-          textAlign: "left",
-          verticalAlign: "baseline",
-          fontSize: 12,
-          color: "#000000",
-          backgroundColor: "#ffffff",
-          fontFamily: "Roboto-Regular",
-          flexDirection: "column",
-          flexWrap: "wrap",
-          justifyContent: "flex-start",
-          alignItems: "stretch",
-          alignContent: "stretch",
-          borderColor: "#000000",
-          borderStyle: "solid",
-          borderWidth: 1,
-          borderRadius: 12,
-          backgroundSize: "auto"
-        }}
-      >
-        <Text
-          style={{
-            marginLeft: 0,
-            marginRight: 0,
-            marginTop: 5,
-            marginBottom: 5,
-            paddingLeft: 5,
-            paddingRight: 5,
-            paddingTop: 5,
-            paddingBottom: 5,
-            overflow: "visible",
-            textAlign: "left",
-            verticalAlign: "baseline",
-            fontSize: 12,
-            color: "#000000",
-            backgroundColor: "#ffffff",
-            fontFamily: "Roboto-Regular",
-            borderColor: "#000000",
-            borderStyle: "solid",
-            borderWidth: 0,
-            borderLeftWidth: 0,
-            borderRightWidth: 0,
-            borderTopWidth: 0,
-            borderBottomWidth: 0,
-            borderRadius: 0,
-            textDecorationLine: "none",
-            textTransform: "none",
-            lineHeight: 12,
-            letterSpacing: 0
-          }}
-        >
-          Sample text content
-        </Text>
+    <View style={this.props.themedStyle.View_1}>
+      <View style={this.props.themedStyle.View_3}>
+        <View style={this.props.themedStyle.View_5}>
+          <Icon
+            iconFont="Eva Design Icons"
+            name="menu"
+            height={20}
+            width={20}
+            style={this.props.themedStyle.Icon_7}
+          />
+        </View>
       </View>
     </View>
   )
 }
 
 export default Blank = withStyles(_Blank, theme => ({
-  container: {
-    backgroundColor: theme["color-basic-100"]
+  View_1: {
+    width: "100%",
+    height: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 2,
+    paddingRight: 2,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    textAlign: "left",
+    verticalAlign: "baseline",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontFamily: "Roboto-Regular",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  View_3: {
+    width: "100%",
+    height: "100%",
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 0,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "visible",
+    textAlign: "left",
+    verticalAlign: "baseline",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontFamily: "Roboto-Regular",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 12,
+    backgroundSize: "auto",
+    alignSelf: "center"
+  },
+  View_5: {
+    width: "100%",
+    height: 5,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 15,
+    marginBottom: 0,
+    paddingLeft: 0,
+    paddingRight: 0,
+    paddingTop: 0,
+    paddingBottom: 0,
+    overflow: "hidden",
+    alignSelf: "flex-start",
+    fontSize: 12,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    flexDirection: "row",
+    flexWrap: "wrap",
+    justifyContent: "flex-start",
+    alignItems: "stretch",
+    alignContent: "stretch",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 1,
+    borderRadius: 0,
+    backgroundSize: "auto"
+  },
+  Icon_7: {
+    height: 100,
+    marginLeft: 0,
+    marginRight: 0,
+    marginTop: 5,
+    marginBottom: 5,
+    paddingLeft: 5,
+    paddingRight: 5,
+    paddingTop: 5,
+    paddingBottom: 5,
+    overflow: "visible",
+    alignSelf: "flex-start",
+    borderColor: "#000000",
+    borderStyle: "solid",
+    borderWidth: 0,
+    borderLeftWidth: 0,
+    borderRightWidth: 0,
+    borderTopWidth: 0,
+    borderBottomWidth: 0,
+    borderRadius: 0,
+    fontSize: 20,
+    color: "#000000",
+    backgroundColor: "#ffffff",
+    fontStyle: "normal",
+    fontWeight: "normal",
+    textAlign: "left",
+    textAlignVertical: "center",
+    textDecorationLine: "none",
+    textTransform: "none",
+    lineHeight: 12,
+    letterSpacing: 0
   }
 }))
